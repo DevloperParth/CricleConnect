d84be1364a5188924f6ac0e6113eba493e0a7d1f
kuldeeplovanshi <kulovanshi@bestpeers.com>
2023-06-06 18:39:50 +0530

    REC-1-added-authentication

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+  default from: 'kuldeeplovanshi777@@gmail.com'
+  def welcome_email(user)
+    @user=user
+    mail to: user.email, subject:"welcome to my site CircleConnet",from: "kuldeeplovanshi777@gmail.com"
+  end
+end
